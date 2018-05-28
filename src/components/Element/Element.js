c5a26c9c15e5e555de7f3b6811ded2c7181e5419
Josh Duck <github@joshduck.com>
2018-05-28 22:16:08 +1000

    Bubbles

--- src/components/Element/Element.js
+++ src/components/Element/Element.js
@@ -13,19 +13,45 @@ const colors = {
   interactive: 320
 };
 
-const Element = ({ element }) => {
-  const color = `hsl(${colors[element.group]}, 85%, 60%)`;
-  return (
-    <div className="Element" style={{ background: color }}>
-      <h2 className="Element-name">{element.name}</h2>
-      {element.group}
-      {/*
-    <p>{element.group}</p>
-    <p>{element.description}</p>
-    <p>{element.links.w3c} {element.links.mdn} {element.links.htmlDoctor}</p>
-    */}
-    </div>
-  );
-};
+const colorFor = element => `hsl(${colors[element.group]}, 83%, 60%)`;
+
+const sizeFor = element => `${1.6 - element.name.length / 20}em`;
+
+class Element extends React.Component {
+  constructor() {
+    super();
+    this.state = { active: false };
+  }
+
+  onBlur(e) {
+    setTimeout(() => this.setState({ active: false }), 150);
+  }
+
+  onFocus() {
+    this.setState({ active: true });
+  }
+
+  render() {
+    const { element } = this.props;
+    const color = colorFor(element);
+    const size = sizeFor(element);
+
+    return (
+      <a
+        href="#"
+        className={"Element " + (this.state.active ? "Element-active" : "")}
+        style={{ background: color }}
+        onClick={e => e.preventDefault()}
+        onFocus={e => this.onFocus(e)}
+        onBlur={e => this.onBlur(e)}
+      >
+        <h2 className="Element-name" style={{ fontSize: size }}>
+          {element.name}
+        </h2>
+        {/*<div className="Element-info">{element.group}</div>*/}
+      </a>
+    );
+  }
+}
 
 export default Element;
