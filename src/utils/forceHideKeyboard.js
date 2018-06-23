aa28cd5c34affc228fbba29bade125f56da0d48b
Josh Duck <github@joshduck.com>
2018-06-23 10:48:49 +1000

    Set styles on input shim correctly

--- src/utils/forceHideKeyboard.js
+++ src/utils/forceHideKeyboard.js
@@ -1,22 +1,20 @@
 /**
- * Fixed positioning will stop scrolling to input.
- * Font size over 16px will prevent Safari zooming in on focus.
- * https://stackoverflow.com/questions/2989263/disable-auto-zoom-in-input-text-tag-safari-on-iphone
- */
-const inputStyle = `
-  opacity: 0;
-  position: fixed;
-  top: -100px;
-  font-size: 20px;`;
-
-/**
  * Create a shim component then focus and unfocus it so that Safari doesn't keep
  * the keyboard open for the iframe that was unceremoniously nuked from the DOM.
  * Note that this will "flash" the keyboard on some (non-iOS) browsers.
  */
 const forceHideKeyboard = () => {
   const input = document.createElement("textarea");
-  input.style = inputStyle;
+
+  /**
+   * Fixed positioning will stop scrolling to input.
+   * Font size over 16px will prevent Safari zooming in on focus.
+   * https://stackoverflow.com/questions/2989263/disable-auto-zoom-in-input-text-tag-safari-on-iphone
+   */
+  input.style.opacity = 0;
+  input.style.position = "fixed";
+  input.style.top = "-100px";
+  input.style.fontSize = "20px";
 
   document.body.prepend(input);
   input.focus();
